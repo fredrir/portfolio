be618f075500699962785b81e845c05b075d4ee8
fredrir <fhansteen@gmail.com>
2024-07-04 15:06:17 +0200

    minor tweak

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
-import { MoonIcon as MoonIconSolid } from "@heroicons/react/24/solid";
-import { SunIcon as SunIconSolid } from "@heroicons/react/24/solid";
+import {
+  MoonIcon as MoonIconSolid,
+  SunIcon as SunIconSolid,
+} from "@heroicons/react/24/solid";
 
 const ThemeToggle = () => {
   const [theme, setTheme] = useState(() => {
     if (typeof window !== "undefined" && window.localStorage) {
-      const storedTheme = window.localStorage.getItem("theme");
-      return storedTheme ? storedTheme : "light";
+      return window.localStorage.getItem("theme") || "light";
     }
     return "light";
   });
@@ -28,18 +29,20 @@ const ThemeToggle = () => {
     <div>
       <div className="hidden md:block">
         <button
+          aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
           className="p-2 transition duration-300 ease-in-out rounded-full group"
           onClick={toggleTheme}
         >
           {theme === "dark" ? (
-            <MoonIconSolid className="w-6 h-6 transition-transform duration-300 ease-in-out text-online-orange group-hover:scale-110" />
+            <MoonIconSolid className="w-6 h-6 transition-transform duration-300 ease-in-out group-hover:scale-110" />
           ) : (
-            <SunIconSolid className="w-6 h-6 transition-transform duration-300 ease-in-out text-online-orange group-hover:scale-110" />
+            <SunIconSolid className="w-6 h-6 transition-transform duration-300 ease-in-out group-hover:scale-110" />
           )}
         </button>
       </div>
       <div className="md:hidden">
         <button
+          aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
           className="w-full p-2 px-4 py-2 text-left text-gray-700 cursor-pointer group dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
           onClick={toggleTheme}
         >
