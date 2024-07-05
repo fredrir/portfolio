6db2d8e44843dbd8fcc6a2115f00240552b9a75f
fredrir <fhansteen@gmail.com>
2024-07-05 15:19:33 +0200

    more realistic terminal

--- components/HomePage/Terminal.tsx
+++ components/HomePage/Terminal.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
 const TerminalComponent = () => {
   const [text, setText] = useState("");
+  const [cursorVisible, setCursorVisible] = useState(true);
+  const [cursorIsFinished, setCursorIsFinished] = useState(false);
   const mainText =
-    "I''m currently in my final year of a Bachelor's degree in Informatics at the Norwegian University of Science and Technology (NTNU) in Trondheim, Norway. Feel free to explore my CV or get in touch with me.";
+    "I'm currently in my final year of a Bachelor's degree in Informatics at the Norwegian University of Science and Technology (NTNU) in Trondheim, Norway. Feel free to explore my CV or get in touch with me.";
   const errorText = "\nCommand not found";
 
   useEffect(() => {
@@ -16,6 +19,7 @@ const TerminalComponent = () => {
       } else {
         clearInterval(interval);
         setTimeout(() => {
+          setCursorIsFinished(true);
           setText((prev) => prev + errorText);
         }, 700);
       }
@@ -23,15 +27,65 @@ const TerminalComponent = () => {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    if (cursorIsFinished) {
+      setCursorVisible(false);
+      return;
+    }
+    const cursorInterval = setInterval(() => {
+      setCursorVisible((prev) => !prev);
+    }, 750);
+    return () => clearInterval(cursorInterval);
+  }, [cursorIsFinished]);
+
   const mainTextLength = mainText.length;
 
   return (
-    <div className="max-w-lg bg-black text-green-500 font-mono p-4 rounded-lg shadow-lg min-h-[180px]">
-      <article className="whitespace-pre-wrap">
-        <span className="text-white">Fredrik:~$ </span>
-        <span>{text.slice(0, mainTextLength)}</span>
-        <span className="text-red-500">{text.slice(mainTextLength)}</span>
-      </article>
+    <div className="flex flex-col">
+      <div className="bg-gray-900 flex flex-row px-3 font-mono text-sm p-1 rounded-t-lg text-white">
+        <p className="mx-auto text-center"></p>
+        <div className="flex flex-row text-end gap-2">
+          <Image
+            src={"/minus-icon.svg"}
+            alt={"minus icon"}
+            width={10}
+            height={10}
+            className="relative top-1"
+          />
+          <Image
+            src={"square-icon.svg"}
+            alt={"square icon"}
+            width={10}
+            height={10}
+          />
+          <Image
+            src={"/close-icon.svg"}
+            alt={"close icon"}
+            width={15}
+            height={15}
+          />
+        </div>
+      </div>
+      <div className="hidden md:flex bg-gray-900 px-3 flex flex-row gap-2 font-mono text-sm text-white">
+        <p>File</p>
+        <p>Edit</p>
+        <p>View</p>
+        <p>Search</p>
+        <p>Terminal</p>
+        <p>Help</p>
+      </div>
+      <div className="max-w-lg bg-black text-green-500 font-mono p-4 rounded-b-lg shadow-lg min-h-[180px]">
+        <article className="whitespace-pre-wrap">
+          <span className="text-white">Fredrik:~$ </span>
+          <span>{text.slice(0, mainTextLength)}</span>
+          <span className="text-red-500">{text.slice(mainTextLength)}</span>
+          {cursorVisible && (
+            <span className="border-white bg-white border border-1 text-white">
+              |
+            </span>
+          )}
+        </article>
+      </div>
     </div>
   );
 };
