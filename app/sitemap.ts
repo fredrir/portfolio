964d2f6abd7c6b02a11eae01f9ba9f5f8d436899
fredrir <fhansteen@gmail.com>
2025-04-11 16:51:25 +0200

    remove french cv from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -49,11 +49,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "monthly",
       priority: 0.8,
     },
-    {
-      url: `${baseUrl}/cv/fr`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
   ];
 }
